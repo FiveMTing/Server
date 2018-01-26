265aa5c72fbca1d4b9ed059f96dfa568a1692f49
JordanNZ <jordan.l.diamond@gmail.com>
2018-01-26 21:34:29 +1300

    updated again

--- resources/main/pingpong.lua
+++ resources/main/pingpong.lua
@@ -1,14 +1,14 @@
 
-AddEventandler ('onResourcesStarting', function( resource )
+AddEventHandler ('onResourcesStarting', function( resource )
     helloWorld()
 end)
 
-AddEventandler ('chatMessageEntered', function ( name, color, message )
+AddEventHandler ('chatMessageEntered', function ( name, color, message )
     if message == "Ping" then
         print ("Print: Pong!")
 end)
 
 function helloWorld()
     print ("Print: Hello World!")
-end)
+end
 
